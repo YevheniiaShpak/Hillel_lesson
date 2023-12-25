73f3581584e9177915f6c1aa81c47ef545b26635
Evgeniia <shpakes86@gmail.com>
2023-12-25 13:12:51 +0200

    ДЗ21

--- lesson12.1.py
+++ lesson12.1.py
@@ -1,11 +1,10 @@
 class Auto:
-    color = "Black"
-    weight = 1500
-
-    def __init__(self, brand="Jeep", age=3, mark="Renegade"):
+    def __init__(self, brand, age, mark, color=None, weight=None):
         self.brand = brand
         self.age = age
         self.mark = mark
+        self.mark = mark
+        self.weight = weight
 
     def move(self):
         print("move")
@@ -17,7 +16,7 @@ class Auto:
         self.age += 1
         print(self.age)
 
-auto_1 = Auto()
+auto_1 = Auto(brand="Jeep", age=3, mark="Renegade", color = "Black", weight = 1500)
 auto_1.move()
 auto_1.stop()
 auto_1.birthday()
